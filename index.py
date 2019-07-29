d97e8968dbae73c25fcc31a42c41b529d92f8336
Bartek <b.laskowski@keepthinking.it>
2019-07-29 09:58:07 +0100

    Get instances informations.

---
+++ index.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+import subprocess
+import json
+import pprint
+
+pp = pprint.PrettyPrinter(depth=6).pprint
+
+set = {
+        'aws': 'aws',
+        'service': 'ec2',
+        'only_tag': 'production'
+        }
+
+def run_command(command):
+    print('\nRunning: ' + command[0] + ' ' + command[1] + ' ' + command[2] + "\n" + '...')
+    res = subprocess.run(command, stdout=subprocess.PIPE)
+    print('Done!')
+    return res.stdout
+
+
+instances = json.loads(run_command(
+    [
+    set['aws'],
+    set['service'],
+    'describe-instances'
+    ]
+    ))['Reservations']
+
+
+def filter_tag_name(tag):
+    return tag['Key'] == "Name"
+
+
+def filtered():
+    out = []
+    for instance in instances:
+        tag = list(filter(filter_tag_name, instance['Instances'][0]['Tags']))
+        o = {
+                "InstanceId": instance['Instances'][0]['InstanceId'],
+                "Tag": tag[0]['Value'],
+                "PublicIpAddress": instance['Instances'][0]['PublicIpAddress'],
+        }
+        if(tag[0]['Value'] == set['only_tag']):
+            out.append(o)
+    return out
+
+pp(filtered())
+
+
+
+def reboot(el):
+    run_command([
+        set['aws'],
+        set['service'],
+        'reboot-instances',
+        'instance-ids=',
+        el['InstanceId']
+        ])
